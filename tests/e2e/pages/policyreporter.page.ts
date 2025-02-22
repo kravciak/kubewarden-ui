3d046c1364d7f17a547b96f57d60223bf7d7ba9c
Martin Kravec <kravciak@gmail.com>
2025-02-22 11:09:51 +0100

    Update tests for policy reporter v3
    Signed-off-by: Martin Kravec <kravciak@gmail.com>

--- tests/e2e/pages/policyreporter.page.ts
+++ tests/e2e/pages/policyreporter.page.ts
@@ -1,21 +1,22 @@
 import { Page, expect } from '@playwright/test'
-import type { Locator, FrameLocator } from '@playwright/test'
+import type { FrameLocator } from '@playwright/test'
 import { BasePage } from '../rancher/basepage'
+import { step } from '../rancher/rancher-test';
 
 export class PolicyReporterPage extends BasePage {
     readonly frame: FrameLocator;
-    readonly failNsBanner: Locator;
-    readonly failNsTable: Locator;
-    readonly failCpBanner: Locator;
-    readonly failCpTable: Locator;
 
     constructor(page: Page) {
       super(page)
       this.frame = page.frameLocator('[data-testid="kw-pr-iframe"]')
-      this.failCpBanner = this.frame.getByText('Failing Cluster Policies', { exact: true }).locator('xpath=./following-sibling::div')
-      this.failNsBanner = this.frame.getByText('Failing Policy Results per Namespace', { exact: true }).locator('xpath=./following-sibling::div')
-      this.failCpTable = this.frame.getByText('Failing ClusterPolicy Results', { exact: true }).locator('xpath=./following-sibling::div//table')
-      this.failNsTable = this.frame.getByText('Failing Policy Results', { exact: true }).locator('xpath=./following-sibling::div//table')
+    }
+
+    getCard(name: 'pass' | 'warn' | 'fail' | 'error') {
+      return this.frame.locator('div.v-card-item').filter({ has: this.page.getByText(name, {exact: true}) }).locator('xpath=./following-sibling::div')
+    }
+
+    getChip(name: string|RegExp, chip: 'pass' | 'warn' | 'fail' | 'error') {
+      return this.frame.locator('a.v-list-item').filter({ has: this.page.getByText(name, {exact: true}) }).locator(`.v-chip.bg-status-${chip}`)
     }
 
     async goto(): Promise<void> {
@@ -23,23 +24,17 @@ export class PolicyReporterPage extends BasePage {
       await this.nav.goto('dashboard/c/local/kubewarden/policy-reporter')
     }
 
-    async selectTab(name: 'Dashboard' | 'Policy Reports' | 'ClusterPolicy Reports' | 'Logs') {
+    @step
+    async selectTab(name: 'Dashboard' | 'Other' | 'Policy Dashboard' | 'Kubewarden') {
       const menu = this.frame.locator('header').locator('i.mdi-menu')
-      const overlay = this.frame.locator('div.v-overlay--active')
       const tabItem = this.frame.locator('nav').getByRole('link', { name, exact: true })
 
-      // Wait until iframe is loaded
-      await expect(this.frame.locator('header')).toBeVisible()
+      // Wait until iframe loads and open menu
+      await expect(menu).toBeVisible()
+      if (!await tabItem.isVisible()) await menu.click()
 
-      // If screen is too small then menu is open with overlay
-      if (await overlay.isVisible() && name === 'Dashboard') {
-        await overlay.click()
-      } else {
-        // Open menu if not visible & select tab
-        if (!await tabItem.isVisible()) await menu.click()
-        await tabItem.click()
-      }
-      // Menu animation can match previous closing nemu
+      // Click and wait for menu animation to not match previous page
+      await tabItem.click()
       await this.page.waitForTimeout(500)
     }
 
